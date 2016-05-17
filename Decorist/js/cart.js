6f21b9fcb7b9ac7b0c8ad7962af8c393ae1a2145
RakelO <zvarun4ik@ukr.net>
2016-05-17 22:01:51 +0300

    new folder

---
+++ Decorist/js/cart.js
@@ -0,0 +1,11 @@
+app.directive('cart', function(){
+	return {
+		templateUrl: 'templates/cart.html',
+		controller: 'CartController',
+		controllerAs: 'CartCtrl'
+	};
+});
+
+app.controller('CartController', function(){
+
+});
